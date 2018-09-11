9c9109510fa219f72e52128c14cdcce990e42200
subashmanohari <subashmanohari@gmail.com>
2018-09-11 23:49:10 +0530

    sum and diff

--- sumanddiffmatrices.py
+++ sumanddiffmatrices.py
@@ -1,7 +1,15 @@
 
-def sumanddiff(cdtm1):
+def summatrix(cdtm1):
     pxpy = np.zeros((160), dtype=int)
     for i in range(0, 81):
         for j in range(0, 81):
             pxpy[i + j] = pxpy[i + j] + cdtm1[i][j]
     return pxpy
+
+
+def diffmatrix(cdtm1):
+    pxmy = np.zeros((80), dtype=int)
+    for i in range(0, 81):
+        for j in range(0, 81):
+            pxmy[abs(i - j)] = pxmy[abs(i - j)] + cdtm1[i][j]
+    return pxmy
