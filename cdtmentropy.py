77af1d900ab3d06abfad4f900b091c5f1702520a
subashmanohari <subashmanohari@gmail.com>
2018-09-12 00:08:36 +0530

    sum and difference entropy

--- cdtmentropy.py
+++ cdtmentropy.py
@@ -6,3 +6,17 @@ def entropy(cdtm1):
         for j in w:
             ans = ans + (cdtm1[i][j] * (math.log2(cdtm1[i][j])))
     return ans
+
+
+def sumentropy(pxpy):
+    ans = 0
+    for i in range(0, 161):
+        ans = ans + (pxpy[i] * (log2(pxpy[i])))
+    return -1 * ans
+
+
+def differenceentropy(pxmy):
+    ans = 0
+    for i in range(0, 81):
+        ans = ans + (pxmy[i] * (log2(pxmy[i])))
+    return -1 * ans
