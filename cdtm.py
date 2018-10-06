74c9d45ba8722dee7371276e17dfd01686f7bf77
Govind <govindchandak14@gmail.com>
2018-10-06 16:26:03 +0530

    divided in blocks

--- cdtm.py
+++ cdtm.py
@@ -89,21 +89,31 @@ print(imgdir)
 for i in imgdir:
         data=os.listdir(path+'/'+i)
         for j in data:
-            c=0
-            df=pd.DataFrame(columns=['index','asm','correlation','variance','idm','entropy','sum_entropy','difference_entropy','sum_avg','contrast','energy','type'])
+            pos=0
+            column=['index']
+            for col in range(1,65):
+                column.extend(('asm'+str(col),'correlation'+str(col),'variance'+str(col),'idm'+str(col),'entropy'+str(col),'sum_entropy'+str(col),'difference_entropy'+str(col),'sum_avg'+str(col),'contrast'+str(col),'energy'+str(col)))
+            df=pd.DataFrame(columns=column)
+            #df=pd.DataFrame(columns=['index','asm','correlation','variance','idm','entropy','sum_entropy','difference_entropy','sum_avg','contrast','energy','asm2','correlation2','variance2','idm2','entropy2','sum_entropy2','difference_entropy2','sum_avg2','contrast2','energy2','asm3','correlation3','variance3','idm3','entropy3','sum_entropy3','difference_entropy3','sum_avg3','contrast3','energy3','asm4','correlation4','variance4','idm4','entropy4','sum_entropy4','difference_entropy4','sum_avg4','contrast4','energy4','asm5','correlation5','variance5','idm5','entropy5','sum_entropy5','difference_entropy5','sum_avg5','contrast5','energy5','asm6','correlation6','variance6','idm6','entropy6','sum_entropy6','difference_entropy6','sum_avg6','contrast6','energy6','asm7','correlation7','variance7','idm7','entropy7','sum_entropy7','difference_entropy7','sum_avg7','contrast7','energy7','asm8','correlation8','variance8','idm8','entropy8','sum_entropy8','difference_entropy8','sum_avg8','contrast8','energy8'])
             for ii in os.listdir(path+'/'+i+'/'+j):
                 print(ii)
                 img=cv2.imread((path+'/'+i+'/'+j+'/'+ii),0)
                 img=np.array(img)
                 #print(img)
-                cdtm1=np.zeros((81,81))
-                cdtm2=cdtm(3,img)
-                for k in range (0,4):
-                    for l in range(0,4):
-                        cdtm1=od2(cdtm2,cdtm1,k,l)
-                cdtm2=CDTMnormalise(cdtm1)
-                df.loc[c]=[c,angular_second_moment(cdtm2),correlation(cdtm2),varianceX(cdtm2),IDM(cdtm2),entropy(cdtm2),sumentropy(cdtm2),differenceentropy(cdtm2),sumAverage(cdtm2),contrast(cdtm2),energy(cdtm2),j]
-                c=c+1
+                row=[pos]
+                for r in range(0,8):
+                    for c in range(0,8):
+                        cdtm1=np.zeros((81,81))
+                        cdtm2=cdtm(3,img[r*32:(r+1)*32,c*32:(c+1)*32])
+                        for k in range (0,4):
+                            for l in range(0,4):
+                                cdtm1=od2(cdtm2,cdtm1,k,l)
+                        cdtm2=CDTMnormalise(cdtm1)
+                        row.extend((angular_second_moment(cdtm2),correlation(cdtm2),varianceX(cdtm2),IDM(cdtm2),entropy(cdtm2),sumentropy(cdtm2),differenceentropy(cdtm2),sumAverage(cdtm2),contrast(cdtm2),energy(cdtm2)))
+               # print(len(row))
+                df.loc[len(df)]=row
+                print(len(df))
+                pos=pos+1
             df.to_csv(str(j)+'.csv',sep=',')
             #break
         #print(data)
